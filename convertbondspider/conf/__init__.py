eaaeae60af82ee0eef174624f9668368ebca1c2a
zhengshuocong <zhengshuocong@zhengshuocongdeMacBook-Pro.local>
2023-10-09 16:08:25 +0800

    chrome_driver 路径修改配置

---
+++ convertbondspider/conf/__init__.py
@@ -0,0 +1,6 @@
+# try:
+# 	from local_config import chromedriver_path
+# except Exception as e:
+# 	chromedriver_path = '/opt/chromedriver/chromedriver-linux64/chromedriver'
+
+chromedriver_path = '/Users/zhengshuocong/Documents/component/chromedriver'
--- convertbondspider/conf/sql_config.py
+++ convertbondspider/conf/sql_config.py
@@ -3,12 +3,22 @@ sys.path.append("..")
 from sqlalchemy import create_engine
 
 
+# class sqlConfig:
+#     db_name = "convert_bond"           # 数据库名
+#     db_user = "root"              # 用户名
+#     db_host = "43.143.142.50"         # IP地址
+#     db_port = 3307                # 端口号
+#     db_passwd = "Aa123456"            # 密码
+
+
 class sqlConfig:
-    db_name = "convert_bond"           # 数据库名
+    db_name = "scripydata"           # 数据库名
     db_user = "root"              # 用户名
-    db_host = "43.143.142.50"         # IP地址
+    db_host = "101.200.85.175"         # IP地址
     db_port = 3307                # 端口号
     db_passwd = "Aa123456"            # 密码
+
+
 
 class sqlExecute():
     """docstring for sqlExecute"""
